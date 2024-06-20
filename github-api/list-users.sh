0afa50897d8ef4b9284ace86fd1d9b1cdbfcc7e1
uday8devops <root@ip-172-31-11-27.ap-south-1.compute.internal>
2024-06-20 09:47:29 +0000

    improved-version-with-helper-function

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#############################
+# About: A Shell script to list out the collaborators of "repo" of "your organizations" section using Github-API
+# 
+# Inputs: Required to export "username" and "token" before executing the script
+#
+# Caution: while executing script pass the 2 command line arguments "your_organizations_name" "your_repo_name"
+#
+# Owner: uday
+# 
+# Date: 20-06-2024
+##############################
+
+
 # GitHub API URL
 API_URL="https://api.github.com"
 
@@ -10,6 +23,7 @@ TOKEN=$token
 # User and Repository information
 REPO_OWNER=$1
 REPO_NAME=$2
+
 
 # Function to make a GET request to the GitHub API
 function github_api_get {
@@ -36,7 +50,25 @@ function list_users_with_read_access {
     fi
 }
 
+# Helper function for user alerts or to know exactly what is the error
+#
+# Function to check command-line arguments
+function helper {
+  expected_cmd_args=2
+  if [ "$#" -ne $expected_cmd_args ];
+  then
+     echo "please execute the script with required command line arguments"
+     echo "organzation name:" and "repo name:"
+     exit 1
+  fi
+}
+
+# Call the helper function to check command-line arguments
+ helper "$@"
+
+
 # Main script
 
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
 list_users_with_read_access
+
